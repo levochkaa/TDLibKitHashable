316ca71c099047f736c0b5d904e780700d46316b
levochkaa <levochkaaa@icloud.com>
2023-06-03 22:42:47 +0300

    Initial Commit

---
+++ TDLibKitHashable/TDLibKitHashableApp.swift
@@ -0,0 +1,12 @@
+// TDLibKitHashableApp.swift
+
+import SwiftUI
+
+@main
+struct TDLibKitHashableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
